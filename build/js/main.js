ce45002bdddcd479aa6e69a0e2f3aaed0a754d4a
PipZip <37911683+PipZip@users.noreply.github.com>
2019-11-25 20:53:02 +0600

    hover

--- build/js/main.js
+++ build/js/main.js
@@ -28,6 +28,19 @@ tpl:'<div class="fancybox-share"><h1>{{SHARE}}</h1><p><a class="fancybox-share__
 $(document).ready(function() {
     $("input[name='phone']").mask(" +7 (999) 999-99-99");
 
+    $('.product__prod-pic, .prod-info').hover(function () {
+       if ($(this).hasClass('product__prod-pic--1')) {
+           $(this).addClass('hovered').siblings('.prod-info--1').addClass('hovered');
+       } else if ($(this).hasClass('product__prod-pic--2')) {
+           $(this).addClass('hovered').siblings('.prod-info--2').addClass('hovered');
+       } else if ($(this).hasClass('prod-info--1')) {
+           $(this).addClass('hovered').siblings('.product__prod-pic--1').addClass('hovered');
+       } else if ($(this).hasClass('prod-info--2')) {
+           $(this).addClass('hovered').siblings('.product__prod-pic--2').addClass('hovered');
+       }
+    }, function () {
+        $('.product__prod-pic, .prod-info').removeClass('hovered');
+    });
 
 
 });
--- src/js/partials/main.js
+++ src/js/partials/main.js
@@ -1,6 +1,19 @@
 $(document).ready(function() {
     $("input[name='phone']").mask(" +7 (999) 999-99-99");
 
+    $('.product__prod-pic, .prod-info').hover(function () {
+       if ($(this).hasClass('product__prod-pic--1')) {
+           $(this).addClass('hovered').siblings('.prod-info--1').addClass('hovered');
+       } else if ($(this).hasClass('product__prod-pic--2')) {
+           $(this).addClass('hovered').siblings('.prod-info--2').addClass('hovered');
+       } else if ($(this).hasClass('prod-info--1')) {
+           $(this).addClass('hovered').siblings('.product__prod-pic--1').addClass('hovered');
+       } else if ($(this).hasClass('prod-info--2')) {
+           $(this).addClass('hovered').siblings('.product__prod-pic--2').addClass('hovered');
+       }
+    }, function () {
+        $('.product__prod-pic, .prod-info').removeClass('hovered');
+    });
 
 
 });
