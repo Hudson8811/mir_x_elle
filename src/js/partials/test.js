e03c9bdf80e9fcb38714abe2d1a9324d95b3932b
PipZip <37911683+PipZip@users.noreply.github.com>
2019-11-29 16:51:59 +0600

    again

--- src/js/partials/test.js
+++ src/js/partials/test.js
@@ -1,34 +1,36 @@
 $(function() {
+    function startTest(){
+        answers = '';
 
-    answers = '';
+        $.getJSON('test.json', function(data) {
+            test = data.test;
+            testCount = test.length;
 
-    $.getJSON('test.json', function(data) {
-        test = data.test;
-        testCount = test.length;
-
-        cur = 1;
-
-
-
-        var text = test[cur-1].text,
-            img1 = test[cur-1].img1,
-            img2 = test[cur-1].img2,
-            img3 = test[cur-1].img3,
-            ans1 = test[cur-1].ans1,
-            ans2 = test[cur-1].ans2,
-            ans3 = test[cur-1].ans3;
-
-        $('#vopros').html(text);
-        $('#querstion .variant-1 img').attr('src',img1);
-        $('#querstion .variant-2 img').attr('src',img2);
-        $('#querstion .variant-3 img').attr('src',img3);
-        $('#querstion .variant-1 .text').html(ans1);
-        $('#querstion .variant-2 .text').html(ans2);
-        $('#querstion .variant-3 .text').html(ans3);
+            cur = 1;
 
 
-        $('#quest-count').html(cur);
-    });
+
+            var text = test[cur-1].text,
+                img1 = test[cur-1].img1,
+                img2 = test[cur-1].img2,
+                img3 = test[cur-1].img3,
+                ans1 = test[cur-1].ans1,
+                ans2 = test[cur-1].ans2,
+                ans3 = test[cur-1].ans3;
+
+            $('#vopros').html(text);
+            $('#querstion .variant-1 img').attr('src',img1);
+            $('#querstion .variant-2 img').attr('src',img2);
+            $('#querstion .variant-3 img').attr('src',img3);
+            $('#querstion .variant-1 .text').html(ans1);
+            $('#querstion .variant-2 .text').html(ans2);
+            $('#querstion .variant-3 .text').html(ans3);
+
+
+            $('#quest-count').html(cur);
+        });
+    }
+    startTest();
 
     $('#querstion .vaiant').click(function () {
         $('#testover').fadeIn(300);
@@ -76,6 +78,14 @@ $(function() {
                 $('#testover').fadeOut(300);
             },300);
         }
+    });
+
+    $('.again-link').click(function () {
+        startTest();
+        $('#otvet').fadeOut(300,function () {
+            $('#querstion').fadeIn(300);
+            $('#testover').fadeIn(300);
+        });
     });
 
 
